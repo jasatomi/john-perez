a3e057e82a1894b18a49de5b1f4f0a13df7fca54
jasatomi <jaja_perez45@yahoo.com>
2017-02-22 15:36:31 +0800

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>Activity 9</title>
+	<script language ="Javascript" type= "text/javascript">
+<!--
+	function getValue(){
+		var ret Val = prompt("Enter your name","your name here")
+		document.write("Hi! + retval")
+
+	}
+</script>
+</head>
+<body>
+<P> Click the following button to see the results</P>
+<form>
+	<Input type="button" value="Click me" onclick="gotValue"/>
+</form>
+</body>
+</html>
---
+++ index3.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+	<title>Machine Problem 5</title>
+<script language="javascript" type="text/javascript">
+<!--
+
+	function evaluated(){
+		var grade = prompt("enter your grade","Enter your grades here");
+
+		if(grade >=50) {
+			document.write("You Passed!");
+		}
+		else{
+			document.write("You Failed");
+	}
+}
+//--
+</script>
+</head>
+<body>
+<P> View your grade.
+<input type = "button" value="Grade" onclick="evaluated()"/>
+</body>
+</html>
